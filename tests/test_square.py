7e94ab17050fd7bbb6642a05ca827908ce70f826
hiroshi.yokoya <hyokoya@gmail.com>
2023-04-15 14:54:18 +0900

    rename math_funcs.py

--- tests/test_square.py
+++ tests/test_square.py
@@ -1,7 +1,19 @@
-from src import square
+from . import math_funcs
 
 
 def test_square():
     x = 2
-    ans = square.square(x)
+    ans = math_funcs.square(x)
     assert ans == 4
+
+
+def test_square_root():
+    x = 4
+    ans = math_funcs.square_root(x)
+    assert ans == 2
+
+
+def test_cube():
+    x = 2
+    ans = math_funcs.cube(x)
+    assert ans == 8
